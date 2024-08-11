027b5f1ecc1fbc1f6c0e24a2e7cf08488e818559
bycarlablau <lopezseguracarla98@gmail.com>
2024-08-11 17:26:11 +0200

    included classes

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ function generatePoem(event) {
       `User instructions: Generate a spanish poem about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    console.log("Generating poem");
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a Spanish poem about ${instructionsInput.value}</div>`;
 
     axios.get(apiUrl).then(displayPoem);
 }
