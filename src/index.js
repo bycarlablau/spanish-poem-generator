0bb37ba113d32579558c9b146e3d864c7a2853e8
bycarlablau <lopezseguracarla98@gmail.com>
2024-08-11 16:40:22 +0200

    Added basic tTypewriter plugin

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    
+    new Typewriter("#poem", {
+      strings: "En las noches claras,",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+}
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
